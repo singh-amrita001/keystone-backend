32b77d65686e34323c08959a9915e57877123d3d
Amrita <as5667287@gmail.com>
2026-07-15 03:41:54 -0400

    Create User entity and repository

---
+++ src/main/java/com/zidio/keystone/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.zidio.keystone.repository;
+
+import com.zidio.keystone.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
